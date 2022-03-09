7eb8f9e310c95b871a92299401f4a69dd7cf5f84
Pooria90 <61358399+Pooria90@users.noreply.github.com>
2022-03-09 09:20:38 +0330

    added downsampling section

--- Lab-1/EEG.m
+++ Lab-1/EEG.m
@@ -79,12 +79,12 @@ title(['Signal on channel ',spec.channelnames{num}])
 
 
 %% ================================================== Part 4: Whole Signal
-offset = max(max(abs(sig)))/2 ;
+offset = max(max(abs(sig)))/2.5 ;
 ElecName = spec.channelnames ;
 disp_eeg(sig,offset,fs,ElecName) ;
 
 
-%% ========================================================== Part 6: DFT
+%% =========================================================== Part 6: DFT
 begs = [2,30,42,50];
 len = 5;
 times = cell(1,length(begs));
@@ -97,7 +97,6 @@ for i = 1:length(begs)
     times{i} = T(idx);
     sigs{i} = ch(idx);
     
-    per = 1/fs;
     N = length(idx); % time points
     dfts{i} = fft(sigs{i});
     
@@ -122,17 +121,21 @@ for i = 1:length(begs)
     title(['Single-Sided Amplitude Spectrum of Partition ',num2str(i)])
     xlabel('Frequency (Hz)')
     ylabel('Amplitude')
+    xlim([0 fs/(2*3)])
 end
 
 
 %% ======================================================== Part 7: Pwelch
 f7 = figure;
 f7.WindowState = 'maximized';
+window = floor(fs/4);
+noverlap = floor(fs/8);
+nfft = floor(fs);
 for i = 1:length(sigs)
-    [pxx, fr] = pwelch(sigs{i},floor(fs/2),floor(fs/4),floor(fs/2),fs,'onesided');
-    %length(fr)
     subplot(2,2,i)
+    [pxx, fr] = pwelch(sigs{i},window,noverlap,nfft,fs,'onesided');
     plot(fr,pxx) 
+%     pspectrum(sigs{i},fs)
     grid on
     title(['Single-Sided Power Spectrum of Partition ',num2str(i)])
     xlabel('Frequency (Hz)')
@@ -140,7 +143,7 @@ for i = 1:length(sigs)
 end
 
 
-%% ======================================================== Part 8: STFT
+%% ========================================================== Part 8: STFT
 f8 = figure;
 f8.WindowState = 'maximized';
 window = 128;
@@ -149,5 +152,48 @@ nfft = 128;
 for i = 1:length(sigs)
     subplot(2,2,i)
     spectrogram(sigs{i},hamming(window),noverlap,nfft,fs,'onesided');
+    xlim([0 fs/(2*Down)])
+    %yticks(linspace(begs(i),begs(i)+5,10))
     title(['Single-Sided Spectogram of Partition ',num2str(i)])
 end
+
+
+%% ================================================== Part 9: Downsampling
+part    = 2; % partition number
+Down    = 3;
+z_lpf   = lowpass(sigs{part},fs/(2*Down),fs);
+z_down  = downsample(z_lpf,Down);
+
+time = downsample(times{part},Down);
+N = length(z_down); % time points
+Y = fft(z_down);
+
+Z2 = abs(Y/N);
+Z1 = Z2(1:floor(N/2)+1);
+Z1(2:end-1) = 2*Z1(2:end-1);
+
+f91 = figure;
+f91.WindowState = 'maximized';
+
+subplot(2,1,1)
+plot(time,z_down)
+grid on
+xlabel('Time (s)')
+ylabel('Amplitude')
+title(['Downsampled Signal on channel ',spec.channelnames{5},' - Partition ',num2str(part)])
+
+subplot(2,1,2)
+fr = (fs/Down)*(0:floor(N/2))/N; % frequency range
+plot(fr,Z1) 
+grid on
+title(['Single-Sided Amplitude Spectrum of Downsampled Partition ',num2str(part)])
+xlabel('Frequency (Hz)')
+ylabel('Amplitude')
+
+f92 = figure;
+f92.WindowState = 'maximized';
+window = floor(128/Down);
+noverlap = floor(64/Down);
+nfft = floor(128/Down);
+spectrogram(z_down,hamming(window),noverlap,nfft,fs/Down,'onesided');
+title(['Single-Sided Spectogram of Downsampled Partition ',num2str(part)])
